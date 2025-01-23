1a6b1c871b790e246f1c12aab228ce9f9cf65d4b
nathaniro <nathaniro102@gmail.com>
2025-01-23 04:03:53 +0100

    django user model

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView, register, CustomLoginView, CustomLogoutView
+from .views import list_books, LibraryDetailView, LoginView, LogoutView
 
 urlpatterns = [
     path("books/", list_books, name="book_list"),  # Function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # Class-based view
     path("register/", views.register, name="register"),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -30,8 +30,8 @@ def register(request):
   return render(request, "relationship_app/register.html", {"form": form})
 
 # Login and Logout views will use Django's built-in views
-class CustomLoginView(LoginView):
+class LoginView(LoginView):
     template_name = "relationship_app/login.html"
 
-class CustomLogoutView(LogoutView):
+class LogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
