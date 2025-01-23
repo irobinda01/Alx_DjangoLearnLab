3b903b6b1d347ae4037206f21427a86df1d8958a
nathaniro <nathaniro102@gmail.com>
2025-01-23 08:37:45 +0100

    advance features and security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from django.http import HttpResponse
+from .forms import ExampleForm
 
 # Create your views here.
 # Groups: Viewers, Editors, Admins
@@ -8,7 +9,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm, ExampleForm
+from .forms import BookForm
 
 @permission_required('your_app.can_view', raise_exception=True)
 def book_list(request):
