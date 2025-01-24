07b29218d5041b3938631f92857709d2165e6831
nathaniro <nathaniro102@gmail.com>
2025-01-24 13:08:57 +0100

    django rest frame work

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework import generics 
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -14,4 +14,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # All Book objects
     serializer_class = BookSerializer  # Serializer for the Book model
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
